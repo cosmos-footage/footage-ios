85a7f90fbdc46dc1753e9672ac2addb5c1557433
Nyeok <dn.shin98@gmail.com>
2026-07-04 22:32:44 +0900

    fix: match first launch tab bar storyboard id

--- Footage/Scene/Home/FL_LetsStartVC.swift
+++ Footage/Scene/Home/FL_LetsStartVC.swift
@@ -18,7 +18,7 @@ class FL_LetsStartVC: UIViewController {
     @IBAction func startButtonPressed(_ sender: UIButton) {
         UserDefaults.standard.set("noPassword", forKey: "UserState")
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        guard let tabBarController = storyboard.instantiateViewController(withIdentifier: "tabBarController") as? UITabBarController else { return }
+        guard let tabBarController = storyboard.instantiateViewController(withIdentifier: "TabBarController") as? UITabBarController else { return }
         view.window?.rootViewController = tabBarController
     }
 }
